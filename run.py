1ea55501171c34fec99f73328edc9bd7b3a433ea
kevin-ci <kevin@codeinstitute.net>
2021-04-11 20:45:24 +0000

    i dislike heroku

--- run.py
+++ run.py
@@ -19,7 +19,8 @@ def show(id):
     info = mongo.db.users.find_one({'_id': ObjectId(id)})
     return info["username"]
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0',
-            port=5000,
-            debug=True)
+if __name__ == "__main__":
+    app.run(
+        host=os.environ.get("IP", "0.0.0.0"),
+        port=int(os.environ.get("PORT", "5000")),
+        debug=True)
