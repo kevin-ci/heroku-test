b76ffea8241e1b26f42ef4636df487b0274fbf8f
kevin-ci <kevin@codeinstitute.net>
2021-04-11 20:35:16 +0000

    basic flask app

---
+++ run.py
@@ -0,0 +1,25 @@
+import os
+
+from bson.objectid import ObjectId
+from flask import Flask, render_template, redirect, request, url_for
+from flask_pymongo import PyMongo
+
+app = Flask(__name__)
+
+app.config['MONGO_DBNAME'] = 'kevin'
+app.config['MONGO_URI'] = os.getenv('MONGO_URI', 'mongodb://localhost')
+mongo = PyMongo(app)
+
+@app.route('/')
+def hello():
+    return "Hello, World"
+
+@app.route('/show/<id>')
+def show(id):
+    info = mongo.db.users.find_one({'_id': ObjectId(id)})
+    return info["username"]
+
+if __name__ == '__main__':
+    app.run(host=os.environ.get('IP', '127.0.0.1'),
+            port=int(os.environ.get('PORT', '8080')),
+            debug=True)
